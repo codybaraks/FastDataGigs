2c0d5a9212fb03fe71c68fb153c9385c6403f069
codybaraks <earvinbaraka@gmail.com>
2019-01-09 00:31:20 +0300

    forms and validation

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, sessions, url_for, flash
+import mysql.connector as connector
+from validation import *
 
-app = Flask(__name__)
+db = connector.connect(host="localhost", user="root", passwd="root", database="Waweru")
+app = Flask(__name__, template_folder='templates')
+app.secret_key = "fsggrsgsrgrg"
 
 
 @app.route('/')
 def home():
-    return 'Hello'
+    return render_template('index.html')
+
+
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    form = ContactForm()
+    if form.validate_on_submit():
+     if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        organization = request.form["organization"]
+        title = request.form["title"]
+        description = request.form["description"]
+
+        print(name, email, phone, organization, title, description)
+        cursor = db.cursor()
+        sql = "INSERT INTO `users`(`name`, `email`, `country`, `phone`, `organization`, `title`, `description`) VALUES (%s,%s,%s,%s,%s,%s)"
+        val = (name, email, phone, organization, title, description)
+        cursor.execute(sql, val)
+        db.commit()
+        flash("saved in database")
+
+    return render_template('formA.html')
 
 
 if __name__ == '__main__':
---
+++ validation.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, Regexp
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, message="Your name is too short")])
+    email = StringField('Email', validators=[DataRequired(), Email(message="Invalid Email")])
+    phone = StringField('Phone', validators=[DataRequired(), Length(min=10, message="short number provided"),
+                                             Regexp('^\d{3}\d{3}\d{4}$',
+                                                    message="You must provide 10 char Phone Number FORMAT [0794..]")])
+
+
+organization = StringField('organization',
+                           validators=[DataRequired(), Length(min=2, message="Your organization is too short")])
+title = StringField('title', validators=[DataRequired(), Length(min=2, message="Your Title is too short")])
+description = StringField('description',
+                          validators=[DataRequired(), Length(min=2, message="Your Description is too short")])
